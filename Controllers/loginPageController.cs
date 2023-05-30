d6c04cc72dccffa9792f0712f68a8a1bf119ab59
JacobParker121 <98373778+JacobParker121@users.noreply.github.com>
2023-05-29 19:41:02 -0600

    login
    added login controller and view also did some of the CSS and HTML for the login screen

---
+++ Controllers/loginPageController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace WelnessWebsite.Controllers;
+
+
+public class LoginPage : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult login()
+    {
+        return View();
+    }
+ 
+}
