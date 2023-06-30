56780f42ee87809cb2830ee05d4511fd2b634a40
parker2340 <danielrecelparker1@gmail.com>
2023-06-29 21:31:02 -0600

    stuff

--- Migrations/20230630031031_newer.Designer.cs
+++ Migrations/20230630031031_newer.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WelnessWebsite.Data;
 
@@ -11,9 +12,10 @@ using WelnessWebsite.Data;
 namespace WelnessWebsite.Migrations
 {
     [DbContext(typeof(WelnessWebsiteContext))]
-    partial class WelnessWebsiteContextModelSnapshot : ModelSnapshot
+    [Migration("20230630031031_newer")]
+    partial class newer
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
